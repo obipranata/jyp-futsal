8a3d8b6c7ab81265fc088985c74b5dfe8ed2a1c7
NandoApri26 <apriando.pratamaa@gmail.com>
2025-01-12 00:49:54 +0700

    Add Route event

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\EventController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -13,6 +14,7 @@ Route::get('/', [BerandaController::class, 'index'])->name('beranda');
 Route::get('/booking', [BookingController::class, 'index'])->name('booking');
 Route::get('/register-admin', [RegisterController::class, 'registerAdmin'])->name('register-admin');
 Route::get('/register-customer', [RegisterController::class, 'registerCustomer'])->name('register-customer');
+Route::get('/event', [EventController::class, 'index'])->name('event');
 
 Route::middleware([
     'auth:sanctum',
